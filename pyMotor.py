e78f4a6d24c5eb27315416dac0772b9e47cee2c9
gerardyy <gerard.yy@gmail.com>
2024-02-28 23:01:30 +0100

    version compiling without the MDD10 enabled

--- pyMotor.py
+++ pyMotor.py
@@ -11,14 +11,14 @@ motor2_dir = 22  # Broche IN1 pour le deuxième moteur
 
 # Configuration de la bibliothèque RPi.GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+GPIO.setwarnings(True)
 
 # Configuration des broches en tant que sortie
 GPIO.setup(motor1_pwm, GPIO.OUT)
-GPIO.setup(motor1_in1, GPIO.OUT)
+GPIO.setup(motor1_dir, GPIO.OUT)
 
 GPIO.setup(motor2_pwm, GPIO.OUT)
-GPIO.setup(motor2_in1, GPIO.OUT)
+GPIO.setup(motor2_dir, GPIO.OUT)
 
 # Création des objets PWM pour contrôler la vitesse des moteurs
 motor1 = GPIO.PWM(motor1_pwm, 100)  # Fréquence de PWM : 100 Hz
@@ -75,24 +75,32 @@ def motor2_stop():
 
 # Test des fonctions de contrôle des moteurs
 try:
+    print ("try")
     while True:
         motor1_forward(50)  # Faire tourner le premier moteur dans le sens horaire à 50% de sa vitesse
         motor2_backward(75)  # Faire tourner le deuxième moteur dans le sens horaire à 75% de sa vitesse
         time.sleep(2)
 
+        print ("stop")
         motor1_stop()  # Arrêter le premier moteur
         motor2_stop()  # Arrêter le deuxième moteur
         time.sleep(1)
 
-        motor1_foward(60)  # Faire tourner le premier moteur dans le sens anti-horaire à 60% de sa vitesse
-        motor2_foward(40)  # Faire tourner le deuxième moteur dans le sens anti-horaire à 40% de sa vitesse
+        print ("forward")
+        motor1_forward(60)  # Faire tourner le premier moteur dans le sens anti-horaire à 60% de sa vitesse
+        motor2_forward(40)  # Faire tourner le deuxième moteur dans le sens anti-horaire à 40% de sa vitesse
         time.sleep(2)
 
+        print ("stop")
         motor1_stop()  # Arrêter le premier moteur
         motor2_stop()  # Arrêter le deuxième moteur
         time.sleep(1)
 
 except KeyboardInterrupt:
+    print ("ctrl C")
+
+finally:
+    print ("finally ")
     motor1.stop()
     motor2.stop()
     GPIO.cleanup()
