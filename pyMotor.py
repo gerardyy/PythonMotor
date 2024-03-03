db0ea659382ff97922107b670c8c8c1e47f596a3
gerardyy <gerard.yy@gmail.com>
2024-03-03 22:13:35 +0100

    Add files via upload
    moiteurs 1 et  pilotés via une fonction motors qui permet de geer les 2 moteurs simultanément

--- pyMotor.py
+++ pyMotor.py
@@ -1,32 +1,54 @@
 import RPi.GPIO as GPIO
 import time
+#Hardware
+# Raspberry PI Model B Rev 2
+# https://www.etechnophiles.com/raspberry-pi-3-gpio-pinout-pin-diagram-and-specs-in-detail-model-b/
+# Software PWM is available on all pins
+# Hardware PWM is available on these pins only: GPIO12, GPIO13, GPIO18, GPIO19
+# Cytron MDD10A 
+
+# On the Raspberry : 
+    # GND          = rang 1 broche 3 -vert jaune vert jaune
+    # PWM1 GPIO_12 = rang 1 broche -5 # blanc blanc - PWM
+    # PWM2 GPIO-13 = rang 2 broche -4 # bleu marron+blanc
+    # DIR1 GPIO_24 = Rang 1 broche 9  # jaune rouge orange 
+    # DIR2 GPIO_26 = Rang 2 broche -2 # vert 
 
 # Définition des broches pour le contrôle du premier moteur
-motor1_pwm = 18  # Broche PWM pour le premier moteur
-motor1_dir = 23  # Broche IN1 pour le premier moteur
+pin_motor1_pwm = 12  # Broche PWM pour le premier moteur
+pin_motor1_dir = 24  # Broche DIR1 pour le premier moteur
 
 # Définition des broches pour le contrôle du deuxième moteur
-motor2_pwm = 17  # Broche PWM pour le deuxième moteur
-motor2_dir = 22  # Broche IN1 pour le deuxième moteur
+pin_motor2_pwm = 13  # Broche PWM pour le deuxième moteur
+pin_motor2_dir = 26  # Broche DIR2 pour le deuxième moteur
 
 # Configuration de la bibliothèque RPi.GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+GPIO.setwarnings(True)
 
-# Configuration des broches en tant que sortie
-GPIO.setup(motor1_pwm, GPIO.OUT)
-GPIO.setup(motor1_in1, GPIO.OUT)
+# Save the speed to decrease the speed smoothly when quitting theprog
+motor1_speed = 0 
+motor2_speed = 0
 
-GPIO.setup(motor2_pwm, GPIO.OUT)
-GPIO.setup(motor2_in1, GPIO.OUT)
+# Configuration des broches en tant que sortie et mettre a zero si jamais une broche était restée HIGH
+GPIO.setup(pin_motor1_pwm, GPIO.OUT)
+GPIO.output(pin_motor1_pwm, GPIO.LOW)
+GPIO.setup(pin_motor1_dir, GPIO.OUT)
+GPIO.output(pin_motor1_dir, GPIO.LOW)
+
+GPIO.setup(pin_motor2_pwm, GPIO.OUT)
+GPIO.output(pin_motor2_pwm, GPIO.LOW)
+GPIO.setup(pin_motor2_dir, GPIO.OUT)
+GPIO.output(pin_motor2_dir, GPIO.LOW)
+time.sleep(1)
 
 # Création des objets PWM pour contrôler la vitesse des moteurs
-motor1 = GPIO.PWM(motor1_pwm, 100)  # Fréquence de PWM : 100 Hz
-motor2 = GPIO.PWM(motor2_pwm, 100)
+motor1 = GPIO.PWM(pin_motor1_pwm, 1000)  # Fréquence de PWM : 1000 Hz
+motor2 = GPIO.PWM(pin_motor2_pwm, 1000)
 
 # Démarrage des objets PWM avec un rapport cyclique de 0 (arrêt)
-motor1.start(0)
-motor2.start(0)
+motor1.start(0) # Set an initial value
+motor2.start(0) # Set an initial value
 
 # Fonction pour faire tourner le premier moteur dans le sens horaire
 def motor1_forward(speed):
@@ -34,8 +56,11 @@ def motor1_forward(speed):
     # Low     X(Don’t care)     Low         Low
     # High    Low               High        Low
     # High    High              Low         High
-    GPIO.output(motor1_dir, GPIO.HIGH)
+    global motor1_speed
+    GPIO.output(pin_motor1_dir, GPIO.HIGH)
     motor1.ChangeDutyCycle(speed)
+    motor1_speed = speed
+    print ("motor1_speed = ",motor1_speed)
 
 # Fonction pour faire tourner le premier moteur dans le sens anti-horaire
 def motor1_backward(speed):
@@ -43,12 +68,27 @@ def motor1_backward(speed):
     # Low     X(Don’t care)     Low         Low
     # High    Low               High        Low
     # High    High              Low         High
-    GPIO.output(motor1_dir, GPIO.LOW)
+    global motor1_speed
+    GPIO.output(pin_motor1_dir, GPIO.LOW)
     motor1.ChangeDutyCycle(speed)
+    motor1_speed = speed
 
 # Fonction pour arrêter le premier moteur
 def motor1_stop():
+    global motor1_speed
     motor1.ChangeDutyCycle(0)
+    motor1_speed = 0
+    motor1.stop()
+
+# Permet d'arreter le moteur progressivement
+def motor1_smooth_stop():
+    global motor1_speed
+    print (motor1_speed)
+    while motor1_speed > 0:
+        print (motor1_speed)
+        motor1.ChangeDutyCycle(motor1_speed)
+        motor1_speed-=0.01
+    motor1.stop()
 
 # Fonction pour faire tourner le 2e  moteur dans le sens horaire
 def motor2_forward(speed):
@@ -56,8 +96,10 @@ def motor2_forward(speed):
     # Low     X(Don’t care)     Low         Low
     # High    Low               High        Low
     # High    High              Low         High
-    GPIO.output(motor2_dir, GPIO.HIGH)
+    global motor2_speed
+    GPIO.output(pin_motor2_dir, GPIO.HIGH)
     motor2.ChangeDutyCycle(speed)
+    motor2_speed = speed
 
 # Fonction pour faire tourner le 2e  moteur dans le sens anti-horaire
 def motor2_backward(speed):
@@ -65,34 +107,51 @@ def motor2_backward(speed):
     # Low     X(Don’t care)     Low         Low
     # High    Low               High        Low
     # High    High              Low         High
-    GPIO.output(motor2_dir, GPIO.LOW)
+    global motor2_speed
+    GPIO.output(pin_motor2_dir, GPIO.LOW)
     motor2.ChangeDutyCycle(speed)
+    motor2_speed = speed
 
 # Fonction pour arrêter le premier moteur
 def motor2_stop():
     motor2.ChangeDutyCycle(0)
+    motor2_speed = 0
+    motor2.stop()
 
+# Permet d'arreter le moteur progressivement
+def motor2_smooth_stop():
+    global motor2_speed
+    while motor2_speed > 0:
+        motor2.ChangeDutyCycle(motor2_speed)
+        motor2_speed-=0.01
+    motor2.stop()
 
-# Test des fonctions de contrôle des moteurs
+# Unique command for both motors at the same time
+def motors_run (speed1,speed2):
+    motor1_backward(speed1)
+    motor2_forward(speed2)
+    
+    
 try:
     while True:
-        motor1_forward(50)  # Faire tourner le premier moteur dans le sens horaire à 50% de sa vitesse
-        motor2_backward(75)  # Faire tourner le deuxième moteur dans le sens horaire à 75% de sa vitesse
-        time.sleep(2)
-
-        motor1_stop()  # Arrêter le premier moteur
-        motor2_stop()  # Arrêter le deuxième moteur
-        time.sleep(1)
-
-        motor1_foward(60)  # Faire tourner le premier moteur dans le sens anti-horaire à 60% de sa vitesse
-        motor2_foward(40)  # Faire tourner le deuxième moteur dans le sens anti-horaire à 40% de sa vitesse
-        time.sleep(2)
-
-        motor1_stop()  # Arrêter le premier moteur
-        motor2_stop()  # Arrêter le deuxième moteur
-        time.sleep(1)
+        print ("FOnd")
+        motors_run (100,100)
+        time.sleep(5)
+        print ("FOnd gauche")
+        motors_run (50,100)
+        time.sleep(5)
+        print ("FOnd droit")
+        motors_run (100,50)
+        time.sleep(5)
+        print ("court")
+        motors_run (50,50)
+        time.sleep(5)
 
 except KeyboardInterrupt:
-    motor1.stop()
-    motor2.stop()
+    print ("ctrl C")
+
+finally:
+    print ("finally ")
+    motor1_smooth_stop()
+    motor2_smooth_stop()
     GPIO.cleanup()
